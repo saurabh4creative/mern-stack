9e095569c1d330c0cebe8c2fc665698a5400da12
saurabhjain9070 <tiwefe7341@0pppp.com>
2022-11-22 06:28:40 +0530

    changes done

--- client/src/_redux/_reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import registerReducer from "./registerReducer";
-import userReducer from "./userReducer";
-
-const RootReducer = combineReducers({
-    registerReducer, userReducer
-})
-
-export default RootReducer;
